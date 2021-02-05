362d1ce1bcdc90ef9d74f9ef2dc3dbbfdda5c008
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-02-05 13:20:26 -0300

    Error Handler

--- src/main/java/com/gabrielf/cursomc/services/CategoriaService.java
+++ src/main/java/com/gabrielf/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.gabrielf.cursomc.domain.Categoria;
 import com.gabrielf.cursomc.repository.CategoriaRepository;
+import com.gabrielf.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria Buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }

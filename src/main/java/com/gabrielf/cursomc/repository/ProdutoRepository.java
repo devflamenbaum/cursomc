dc07af21cb8cf3447e8708627be5dcf799bb050c
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-02-05 12:49:52 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/gabrielf/cursomc/repository/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.gabrielf.cursomc.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabrielf.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}

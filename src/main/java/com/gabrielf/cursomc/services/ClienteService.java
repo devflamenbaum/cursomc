863b6407476e02d6f1b60a29f3a265843d8d9a8c
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-02-06 08:27:41 -0300

    Endpoint cliente

---
+++ src/main/java/com/gabrielf/cursomc/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.gabrielf.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gabrielf.cursomc.domain.Cliente;
+import com.gabrielf.cursomc.repository.ClienteRepository;
+import com.gabrielf.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente Buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}

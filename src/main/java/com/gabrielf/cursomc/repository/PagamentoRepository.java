bcf307fcfc19484c249ae243bf657f54a8e4cdfc
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-02-06 09:53:01 -0300

    Pedido, EstadoPagamento, Pagamento

---
+++ src/main/java/com/gabrielf/cursomc/repository/PagamentoRepository.java
@@ -0,0 +1,13 @@
+package com.gabrielf.cursomc.repository;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gabrielf.cursomc.domain.Pagamento;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+
+}

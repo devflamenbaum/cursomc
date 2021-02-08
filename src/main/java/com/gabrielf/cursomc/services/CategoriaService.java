a078c3e05f1f07a5c65f61f81e6c961bd21d3c03
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-02-08 20:39:16 -0300

    Validação Sintatica

--- src/main/java/com/gabrielf/cursomc/services/CategoriaService.java
+++ src/main/java/com/gabrielf/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gabrielf.cursomc.domain.Categoria;
+import com.gabrielf.cursomc.dto.CategoriaDTO;
 import com.gabrielf.cursomc.repository.CategoriaRepository;
 import com.gabrielf.cursomc.services.exceptions.DataIntegrityException;
 import com.gabrielf.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage,Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }

0d81d72e326c3d027d244d9cac3db2ec9e9a5fe6
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-02-11 19:58:26 -0300

    Validação Personalizada

--- src/main/java/com/gabrielf/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/gabrielf/cursomc/dto/ClienteNewDTO.java
@@ -2,20 +2,42 @@ package com.gabrielf.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.gabrielf.cursomc.services.validation.ClientInsert;
+
+@ClientInsert
 public class ClienteNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@NotEmpty(message = "preenchimento obrigatório.")
+	@Length(min=5,max=120,message = "O Campo deve ter entre 5 e 120 caracteres.")
 	private String nome;
+	
+	@NotEmpty(message = "preenchimento obrigatório.")
+	@Email(message = "email inválido.")
 	private String email;
+	
+	@NotEmpty(message = "preenchimento obrigatório.")
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
+	@NotEmpty(message = "preenchimento obrigatório.")
 	private String logradouro;
+	@NotEmpty(message = "preenchimento obrigatório.")
 	private String numero;
+	
 	private String complemento;
+
 	private String bairro;
+	
+	@NotEmpty(message = "preenchimento obrigatório.")
 	private String cep;
 	
+	@NotEmpty(message = "preenchimento obrigatório.")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;

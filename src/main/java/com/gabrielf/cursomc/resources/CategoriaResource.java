6f7674f9435fc7c2c2506d6522f75e153d30b06b
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-02-13 09:47:54 -0300

    Busca de pedido por nome e categoria

--- src/main/java/com/gabrielf/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/gabrielf/cursomc/resources/CategoriaResource.java
@@ -70,7 +70,7 @@ public class CategoriaResource {
 			@RequestParam(value = "page",defaultValue = "0") Integer page, 
 			@RequestParam(value = "linesPerPage",defaultValue = "24")Integer linesPerPage, 
 			@RequestParam(value = "orderBy",defaultValue = "nome")String orderBy, 
-			@RequestParam(value = "direction",defaultValue = "ASC")String direction) {		
+			@RequestParam(value = "direction",defaultValue = "ASC")String direction) {
 		Page<Categoria> listaCategorias = service.findPage(page,linesPerPage,orderBy,direction);
 		Page<CategoriaDTO> listaDTO = listaCategorias.map(obj -> new CategoriaDTO(obj));
 		return ResponseEntity.ok().body(listaDTO);

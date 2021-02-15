d8176570852fd843f17685622d627a31a256178a
Gabriel Flamenbaum <bielflam.engpet@gmail.com>
2021-02-15 00:02:07 -0300

    implementando smtpEmailService

--- src/main/java/com/gabrielf/cursomc/config/DevConfig.java
+++ src/main/java/com/gabrielf/cursomc/config/DevConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.gabrielf.cursomc.services.DBService;
+import com.gabrielf.cursomc.services.EmailService;
+import com.gabrielf.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -15,7 +17,7 @@ public class DevConfig {
 	@Autowired
 	private DBService service;
 	
-	@Value("{spring.jpa.hibernate.ddl-auto}")
+	@Value("${spring.jpa.hibernate.ddl-auto}")
 	private String strategy;
 	
 	@Bean
@@ -27,6 +29,11 @@ public class DevConfig {
 		
 		service.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
